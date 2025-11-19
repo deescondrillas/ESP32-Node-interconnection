f6e8fa0495e54a7aff22b81578da26bc50b8884c
Belsy15 <a01739326@tec.mx>
2025-11-19 10:17:39 -0600

    Update config.cpp

--- ESP32_Code/src/config.cpp
+++ ESP32_Code/src/config.cpp
@@ -7,7 +7,7 @@ const char* WIFI_PASSWORD = "spotless.magnetic.bridge";
 // MQTT / ThingSpeak
 const char* MQTT_SERVER     = "192.168.x.x";   // IP de tu laptop
 const char* MQTT_USER       = "";
-const char* MQTT_CLIENT_ID  = "";
+const char* MQTT_CLIENT_ID = "ESP32_TEST";
 const char* MQTT_PASS       = "";
 
 
