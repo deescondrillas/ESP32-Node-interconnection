fde372614664ecce4e0c380a4823dab685cb48fe
Charls-Ferat <a01739190@tec.mx>
2025-11-24 02:13:46 -0600

    minor details 2.0

--- Publisher_Node_/config.h
+++ Publisher_Node_/config.h
@@ -3,18 +3,18 @@
 #include "passwords.h"
 
 // WiFi
-const char* WIFI_SSID_VAR     = "Totalplay-149F";
-const char* WIFI_PASSWORD_VAR = "149F5250gtYFVWDx";
+const char* WIFI_SSID_VAR     = "Tec-IoT";                      //Network name
+const char* WIFI_PASSWORD_VAR = "spotless.magnetic.bridge";     //Network password
 
 // MQTT / ThingSpeak
-const char* MQTT_SERVER   = "mqtt3.thingspeak.com";
-const int   MQTT_PORT     = 1883;  // Non-secure MQTT
-const char* MQTT_USER_VAR      = "JQs9ByoEBi0RBSMtCxgRBwI";
-const char* MQTT_CLIENT_ID_VAR = "JQs9ByoEBi0RBSMtCxgRBwI";
-const char* MQTT_PASS_VAR      = "LPvMo6GgwXSs3JO4apF49akX";
+const char* MQTT_SERVER   = "";         // MQTT Server direction - local IPv4
+const int   MQTT_PORT     = 1883;       // Non-secure MQTT
+const char* MQTT_USER_VAR      = "";    // MQTT passwords - Non required for now
+const char* MQTT_CLIENT_ID_VAR = "";
+const char* MQTT_PASS_VAR      = "";
 
 // Channel ID
-#define CHANNEL_ID 3150959
+#define CHANNEL_ID "test"               // Define channel as test channel
 
 // Device ID
 const char* DEVICE_ID = "ESP32_01";
